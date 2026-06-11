e2d85690b52e28f62ea104946ac69c3f1683aba7
Abdulrahman Elhabal <abdelrahmanelhabal910@gmail.com>
2026-06-11 21:32:48 +0300

    game theory

---
+++ Game theory/Grundy/Grundy.cpp
@@ -0,0 +1,14 @@
+int MEX(vector<int> v){
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    int mex = 0;
+    while (mex < v.size() && v[mex] == mex) ++mex;
+    return mex;
+}
+int grundy(int x) {
+    if (x == 0) return 0;
+    // remember to memo
+    vector<int> v;
+    // fill v with the grundy(nx) where nx is any next position you can go to
+    return MEX(v);
+}
