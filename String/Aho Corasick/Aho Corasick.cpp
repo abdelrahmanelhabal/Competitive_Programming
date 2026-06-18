5a345cc970d62033b200a333031310a0f98d9e2d
Abdulrahman Elhabal <abdelrahmanelhabal910@gmail.com>
2026-06-18 20:37:21 +0300

    change

--- String/Aho Corasick/Aho Corasick.cpp
+++ String/Aho Corasick/Aho Corasick.cpp
@@ -1,50 +1,58 @@
-struct AC {
-    int N ;
-    const int A = 26;
-    vector <vector <int>> next;
-    vector <int> link, out_link;
-    vector <vector <int>> out;
-    AC(): N(0) {
+struct AHO{
+    int N ; 
+    static const int A = 26 ; 
+    static const int MAXN = 200005 ;
+ 
+    int link[MAXN] , out_link[MAXN];
+    int nxt[MAXN][A];
+    vector<int>  out[MAXN] , adj[MAXN];
+ 
+    AHO():N(0){
         node();
     }
-    int node() {
-        next.emplace_back(A, 0);
-        link.emplace_back(0);
-        out_link.emplace_back(0);
-        out.emplace_back(0);
+ 
+    int node(){
+        fill(nxt[N], nxt[N] + A, 0);
+        link[N] = 0;
+        out_link[N] = 0;
+        out[N].clear();
         return N++;
     }
-    inline int get (char c) {
+ 
+    int get(char c){
         return c - 'a';
     }
-    void add_pattern (const string T, int idx) {
-        int u = 0;
-        for (auto c : T) {
-            if (!next[u][get(c)]) next[u][get(c)] = node();
-            u = next[u][get(c)];
+ 
+    int insert(string &s , int idx){
+        int u = 0 ; 
+        for(auto c : s){
+            if(nxt[u][get(c)] == 0) nxt[u][get(c)] = node();
+            u = nxt[u][get(c)];
         }
-        out[u].push_back(idx);
+        out[u].emplace_back(idx);
+        return u ; 
     }
-    void compute() {
-        queue<int> q;
-        for (int c = 0; c < A; ++c)
-            if (next[0][c]) q.push(next[0][c]);
-        while (!q.empty()) {
-            int u = q.front(); q.pop();
-            for (int c = 0; c < A; ++c) {
-                int v = next[u][c];
-                if (!v) next[u][c] = next[link[u]][c];
-                else {
-                    link[v] = next[link[u]][c];
+ 
+    void build(){
+        queue<int> q; 
+        for(int c = 0 ; c < A ; c++){
+            if(nxt[0][c]) q.emplace(nxt[0][c]);
+        }
+        while(q.size()){
+            auto u = q.front(); q.pop();
+            for(int c = 0 ; c < A ; c++){
+                int v = nxt[u][c] ; 
+                if(v == 0) nxt[u][c] = nxt[link[u]][c];
+                else{
+                    link[v] = nxt[link[u]][c];
                     out_link[v] = out[link[v]].empty() ? out_link[link[v]] : link[v];
-                    q.push(v);
+                    q.emplace(v);
                 }
-            }
+            }            
         }
     }
-    int advance (int u, char c) {
-        while (u && !next[u][get(c)]) u = link[u];
-        u = next[u][get(c)];
-        return u;
-    }
+ 
+    int advance(int u , char c){
+        return nxt[u][get(c)]; 
+    } 
 };
